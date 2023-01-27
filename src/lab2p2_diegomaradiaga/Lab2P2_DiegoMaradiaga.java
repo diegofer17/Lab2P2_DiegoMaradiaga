09f0b6c76854a73d2f2daeec9334d86679bfd667
diegofer17 <df346891@gmail.com>
2023-01-27 13:39:50 -0600

    Main con menu

---
+++ src/lab2p2_diegomaradiaga/Lab2P2_DiegoMaradiaga.java
@@ -0,0 +1,32 @@
+package lab2p2_diegomaradiaga;
+
+import java.util.Scanner;
+
+public class Lab2P2_DiegoMaradiaga {
+    
+    static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+        int opcion;
+        do{
+            System.out.println("--------------------------------------------");
+            System.out.println(">>>>>>>   Inmbobilaria BLACK_FLOYD   <<<<<<<");
+            System.out.println("");
+            System.out.println("1. Registro de Inmbueble/Solar ");
+            System.out.println("2. Manejo de Estados           ");
+            System.out.println("3. Log In/Sign Up              ");
+            System.out.println("4. Salir ");
+            System.out.println("");
+            System.out.print("Ingrese su opcion: ");
+            opcion = leer.nextInt();
+            System.out.println("");
+            
+            
+            switch (opcion){
+                case 1:{ 
+                    
+                }
+            }
+        }while(opcion!=4);
+    }
+    
+}
